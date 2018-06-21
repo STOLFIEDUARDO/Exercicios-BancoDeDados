ae523b350d06cf3a22b27b7ca577ce487592f775
STOLFIEDUARDO <38699786+STOLFIEDUARDO@users.noreply.github.com>
2018-06-21 17:19:44 -0300

    Finalização primeira lista Banco de dados

---
+++ EXERCICIO INSERT 03.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE champions(
+
+	nome VARCHAR(50),
+	descricao VARCHAR(100),
+	habilidade1 VARCHAR(100),
+	habilidade2 VARCHAR(100),
+	habilidade3 VARCHAR(100),
+	habilidade4 VARCHAR(100),
+	habilidade5 VARCHAR(100)
+	
+
+
+);
+
+INSERT INTO champions VALUES('Katarina', 'a Lâmina Sinistra', 'Voracidade', 'Lâmina Saltitante', 'Preparação','Shunpo' , 'Lótus da Morte');
+INSERT INTO champions(nome,habilidade1,habilidade2,habilidade3,habilidade4,habilidade5) VALUES('Yasuo', 'Estilo do Errante', 'Tempestade de Aço', 'Parede de Vento', 'Espada Ágil','Último Suspiro');
+INSERT INTO champions(nome,descricao,habilidade1) VALUES('Master Yi', 'o Espadachim Wuju', 'Ataque Duplo');
+INSERT INTO champions VALUES('Vayne', 'a Caçadora Noturna', 'Caçadora Noturna', 'Rolamento', 'Dardos de Prata','Condenar' , 'Hora Final');
+INSERT INTO champions(nome,descricao,habilidade1,habilidade2,habilidade3,habilidade4) VALUES('Lee Sin', 'o Monge Cego', 'Agitação', 'Onda Sônica /
+Ataque Ressonante', 'Proteger /
+Vontade de Ferro','Tempestade /
+Mutilar');
+INSERT INTO champions(nome,descricao,habilidade1,habilidade3,habilidade4,habilidade5) VALUES('Vi', 'a Defensora de
+Piltover', 'Blindagem', 'Pancada Certeira', 'Força
+Excessiva','Saque e
+Enterrada');
+INSERT INTO champions VALUES('Diana', 'o Escárnio da Lua', 'Espada de Prata
+Lunar', 'Golpe Crescente', 'Cascata Lívida','Colapso
+Minguante' , 'Zênite Lunar');
+INSERT INTO champions VALUES('Annie', 'a Criança Sombria', 'Piromania', 'Desintegrar', 'Incinerar','Escudo
+Fundido' , 'Invocar:
+Tibbers');
+INSERT INTO champions(nome,habilidade1,habilidade2,habilidade3,habilidade4,habilidade5) VALUES('Aatrox', 'Poço de Sangue', 'Voo Sombrio', 'Sede de
+Sangue / Preço
+em Sangue', 'Lâminas da
+Aflição','Massacre');
---
+++ Exercicio INSERT 01.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE endereco(
+
+	estado CHAR(2),
+	cidade VARCHAR(140),
+	bairro VARCHAR(120),
+	cep CHAR(10),
+	logradouro VARCHAR(250),
+	número SMALLINT,
+	complemento TEXT
+
+
+);
+
+INSERT INTO endereco VALUES('AC', 'Rio Branco', 'Ayrton Senna', '69.911-866', 'Estrada Deputado José Rui da Silveira Lino', 282, 'Casa');
+INSERT INTO endereco(estado, cidade, bairro, cep, número) VALUES('SC', 'Biguaçu', 'Fundos', '88.161-400',995);
+INSERT INTO endereco VALUES('MG', 'Santa Luzia', 'Padre Miguel', '33.082-050', 'Rua Buenos Aires',371 , 'Apartamento');
+INSERT INTO endereco(estado,bairro,cep,logradouro,número) VALUES('BA', 'São Tomé de
+Paripe', '40.800-361', 'Travessa Luís Hage',685);
+INSERT INTO endereco(estado,cidade,bairro,logradouro,número) VALUES('MG', 'Ipatinga', 'Vila Celeste', 'Rua Antônio Boaventura Batista',645);
+INSERT INTO endereco(estado,cidade,bairro,cep,número) VALUES('RS', 'Passo Fundo', 'Nenê Graeff', '99.030-250',154);
+INSERT INTO endereco VALUES('AM', 'Manaus', 'Petrópolis', '69.079-300', '69.079-300',264 , 'Fundos');
+INSERT INTO endereco(estado,cidade,bairro,cep,logradouro,número) VALUES('TO', 'Gurupi', 'Muniz Santana', '77.402-130', 'Rua Adelmo Aires Negri', 794);
+INSERT INTO endereco(estado,bairro,logradouro,número,complemento) VALUES('AC', 'Preventório', 'Beco da Ligação II',212, 'Bloco B');
+INSERT INTO endereco(estado,cidade,bairro,cep,logradouro,número) VALUES('AP', 'Santana', 'Comercial', '68.925-073', 'Rua Calçoene',648 );
+INSERT INTO endereco(estado,cidade,bairro,cep,logradouro,número) VALUES('PB', 'Cabedelo', 'Camalaú', '58.103-052', 'Rua Siqueira Campos',249);
